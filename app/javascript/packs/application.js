4aa4df53e4fa25a9d935abdd09b89fc3486c8888
bc2mighty <akinadetunmise8@gmail.com>
2021-04-04 05:02:22 +0100

    Fetched First Page of Users Asynchronously

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,8 +13,53 @@ Turbolinks.start()
 ActiveStorage.start()
 
 $(document).ready(function() {
-    $('#table').DataTable( {
-        autoFill: true,
-        paging: false
-    });
+    let api_url = 'http://127.0.0.1:3000/api/v1';
+    let pages = 1;
+    let users = []
+
+    // $('#table').DataTable( {
+    //     autoFill: true,
+    //     paging: false,
+    //     data: data,
+    //     columns: [
+    //         { data: 'name' },
+    //         { data: 'email' },
+    //         { data: 'phone' },
+    //         { data: 'title' }
+    //     ]
+    
+    // });
+
+    $.ajax({
+        url: `${api_url}/users?page=1`,
+        method: 'GET',
+        success: function(data) {
+            pages = parseInt(data.pages)
+            let count = 0
+            data.data.forEach(user => {
+                let tr = `
+                    <tr>
+                        <td>${++count}</td>
+                        <td>${user.name}</td>
+                        <td>${user.email}</td>
+                        <td>${user.phone}</td>
+                        <td>${user.title}</td>
+                        <td><span style="color:${user.status ? 'green">active' : 'red">inactive'}</span></td>
+                        <td></td>
+                    </tr>
+                `
+                $(`tbody`).append(tr)
+            })
+
+            let link_tags = ``
+            for(let i = 1; i <= pages; i++) {
+                if(i > 10) break
+                link_tags += `<a class="page-link page-number">${i}</a>`
+            }
+            $(link_tags).insertAfter($(`.page-append`))
+        },
+        error: function(e) {
+            console.log(e);
+        }
+    })
 })
